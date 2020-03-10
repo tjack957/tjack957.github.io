9af5726482e075b57a26a875ae0f6fa730915f67
tjack957 <58753664+tjack957@users.noreply.github.com>
2020-03-09 22:11:16 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,277 +1,146 @@
-var AM = new AssetManager();
 
-function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
-    this.spriteSheet = spriteSheet;
-    this.startX = startX;
-    this.startY = startY;
-    this.frameWidth = frameWidth;
-    this.frameDuration = frameDuration;
-    this.frameHeight = frameHeight;
-    this.frames = frames;
-    this.totalTime = frameDuration * frames;
-    this.elapsedTime = 0;
-    this.loop = loop;
-    this.reverse = reverse;
-}
-
-Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
-    var scaleBy = scaleBy || 1;
-    this.elapsedTime += tick;
-    if (this.loop) {
-        if (this.isDone()) {
-            this.elapsedTime = 0;
-        }
-    } else if (this.isDone()) {
-        return;
-    }
-    var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
-    var vindex = 0;
-    if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
-        index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
-        vindex++;
-    }
-    while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
-        index -= Math.floor(this.spriteSheet.width / this.frameWidth);
-        vindex++;
-    }
-
-    var locX = x;
-    var locY = y;
-    var offset = vindex === 0 ? this.startX : 0;
-    ctx.drawImage(this.spriteSheet,
-                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
-                  this.frameWidth, this.frameHeight,
-                  locX, locY,
-                  this.frameWidth * scaleBy,
-                  this.frameHeight * scaleBy);
-	
-}
-
-Animation.prototype.currentFrame = function () {
-    return Math.floor(this.elapsedTime / this.frameDuration);
-}
-
-Animation.prototype.isDone = function () {
-    return (this.elapsedTime >= this.totalTime);
-}
+// GameBoard code below
 
 
-// no inheritance
-function Background(game, spritesheet) {
-    this.x = 0;
-    this.y = 0;
-    this.spritesheet = spritesheet;
-    this.game = game;
-    this.ctx = game.ctx;
-};
-
-Background.prototype.draw = function () {
-    this.ctx.drawImage(this.spritesheet,
-                   this.x, this.y);
-    this.ctx.fillStyle = "SaddleBrown";
-    this.ctx.fillRect(0,550,800,250);
-};
-
-Background.prototype.update = function () {
-};
-function Platform(game, spritesheet, x, y) {
-	this.plat = new Animation(spritesheet, 60, 0, 350, 150, 0.1, 1, true, false);
-    this.x = x;
-    this.y = y;
-    this.spritesheet = spritesheet;
-    this.game = game;
-    this.ctx = game.ctx;
+function Circle(game, wire, r, c) {
+   
+    this.radius = r;
+    
+    this.color = c
+    this.wire = wire
+    this.x = wire.x2
+    this.y = wire.y2
     Entity.call(this, game, this.x, this.y);
-};
-Platform.prototype = new Entity();
-Platform.prototype.constructor = Platform;
-Platform.prototype.draw = function () {
-	this.plat.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 0.5);
-  
+
+   
 };
 
-Platform.prototype.update = function () {
-	Entity.prototype.update.call(this);
-};
+Circle.prototype = new Entity();
+Circle.prototype.constructor = Circle;
 
-
-
-// inheritance 
-function Warrior(game, spritesheet) {
-    this.animation = new Animation(spritesheet, 4, 0, 80,70, 0.1, 9, true, false);
-    this.reverseAnimation = new Animation(spritesheet, 4, 808, 80,70, 0.1, 9, true, false);
-    this.up = new Animation(spritesheet, 4, 640, 80,70, 0.1, 7, false, false);
-    this.reverseUp = new Animation(spritesheet, 4, 720, 80,70, 0.1, 7, false, false);
-    this.mine = new Animation(spritesheet, 4, 560, 80,70, 0.1, 7, false, false);
-    this.duck = new Animation(spritesheet, 4, 240, 80,70, 0.1, 5, false, false);
-    this.reverseDuck = new Animation(spritesheet, 4, 170, 77,70, 0.1, 5, false, false);
-    this.speed = 100;
-    this.ctx = game.ctx;
-    this.right = true
-    this.left = false 
-    this.upTest = false
-    this.reverseUpTest = false
-    this.mineTest = false
-    this.duckTest = false
-    this.reverseDuckTest = false
-    this.count = 0;
-    this.time = this.up.elapsedTime
-    this.total = this.up.totalTime
-    this.rightCheck = true
-    this.leftCheck = false
-    Entity.call(this, game, 100, 450);
-}
-
-Warrior.prototype = new Entity();
-Warrior.prototype.constructor = Warrior;
-
-Warrior.prototype.update = function () {
-	
-	if(this.x === 96){
-		this.mine.elapsedTime = 0
-		this.up.elapsedTime = 0
-		this.duck.elapsedTime = 0
-		this.reverseDuck.elapsedTime = 0
-		this.rightCheck = true
-	    this.leftCheck = false
-		this.reverseUpTest = true
-		this.left = false
-	}
-	if (this.x === 600){
-		this.mine.elapsedTime = 0
-		this.reverseUp.elapsedTime = 0
-		this.duck.elapsedTime = 0
-		this.reverseDuck.elapsedTime = 0
-		this.rightCheck = false
-	    this.leftCheck = true
-		this.upTest = true
-		this.right = false
-	}
-	if(this.x === 260){
-		this.mineTest = true
-		this.right = false
-		this.left = false
-
-		
-	}
-	if(this.x === 460){
-		this.right = false
-		this.left = false
-		if(this.rightCheck){
-			this.duckTest = true
-		}
-		if(this.leftCheck){
-			this.reverseDuckTest = true
-		}
-		
-	}
-	if(this.right){
-		this.x += 2;
-	} 
-	if (this.left){
-		this.x -= 2;
-	}
-	if(this.duckTest){
-		if(this.duck.isDone()){
-			this.duckTest = false			
-			this.x += 4
-				
-			this.right = true
-		}
-	}
-	if(this.reverseDuckTest){
-		if(this.reverseDuck.isDone()){
-			this.reverseDuckTest = false			
-			this.x -= 4
-				
-			this.left = true
-		}
-	}
-	if(this.mineTest){
-		if(this.mine.isDone()){			
-			this.mineTest = false			
-			if(this.rightCheck){
-				this.x += 4
-				
-			    this.right = true
-			}
-			if(this.leftCheck){
-				this.x -= 4
-				
-			    this.left = true
-			}
-		}
-	}
-	if(this.upTest){
-		if(this.up.isDone()){			
-			this.upTest = false			
-			this.left = true			
-		}
-		
-	}
-	if(this.reverseUpTest){		
-		var test2 = this.reverseUp.isDone()
-		if(test2){
-			this.reverseUpTest = false
-			this.right = true
-		}
-		
-	}
-	
+Circle.prototype.update = function () {
+	 this.x = this.wire.x2
+	 this.y = this.wire.y2
     Entity.prototype.update.call(this);
-    
-}
+ 
+};
 
-Warrior.prototype.draw = function () {
+Circle.prototype.draw = function (ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = "Red";
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.closePath();
+
+};
+
+function Wire(game) {
 	
-	if(this.right){	
-		this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1.5);
-		
-	} 
-	if(this.left){
-	    this.reverseAnimation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1.5);
-	}
+	this.g =  1
+	this.m1 = 40
+	this.m2 = 40
 	
-	if(this.upTest){
-		this.up.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1.5);
-	}
-	if(this.reverseUpTest){
-		this.reverseUp.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1.5);
-	}
-	if(this.mineTest){
-		this.mine.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1.5);
-	}
-	if(this.duckTest){
-		this.duck.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1.5);
-	}
-	if(this.reverseDuckTest){
-		this.reverseDuck.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1.5);
-	}
-    Entity.prototype.draw.call(this);
-    
+	this.l1 = 200
+	this.l2 = 200
+	
+	this.t1 = Math.PI/2			//Theta Angle
+	this.t2 = Math.PI/2
+	this.t1V = 0.1				//Velocity
+	this.t2V = 0.1
+	this.t1A = 0.001			//Aceleration 
+	this.t2A = 0.002
+	
+	this.x1 = this.l1 * Math.sin(this.t1)
+	this.y1 = -this.l1 * Math.cos(this.t1)
+	
+	this.x2 = this.x1 + this.l2 * Math.sin(this.t2)
+	this.y2 = this.y1 - this.l2 * Math.cos(this.t2)
+	
+	Entity.call(this, game)
 }
+Wire.prototype = new Entity();
+Wire.prototype.constructor = Wire;
+Wire.prototype.update = function (){
+	
+	//Numerator of the Acceleration 1 Formula
+	var top11 = -this.g * (2 * this.m1 + this.m2) * Math.sin(this.t1) 
+	var top12 = -this.m2 * this.g * Math.sin(this.t1 - 2 * this.t2)
+	var top121 = this.t2V * this.t2V * this.l2
+	var top122 = this.t1V * this.t1V * this.l1 * Math.cos(this.t1 - this.t2)
+	var top13 = - 2 * Math.sin(this.t1 - this.t2) * this.m2 * (top121 + top122)
+	
+	//Denominator of the Acceleration formula
+	var bot112 = this.m2 * Math.cos(2 * this.t1 - 2 * this.t2)
+	var bot = (2 * this.m1 + this.m2 - bot112)
+	
+	this.t1A = (top11 + top12 + top13)/(this.l1 * bot)//Acceleration 1 Calculated
+	
+	
+	//Numerator of the Acceleration 2 Formula
+	var top21 = 2 * Math.sin(this.t1 - this.t2)
+	var top22 = this.t1V * this.t1V * this.l1 * (this.m1 + this.m2)
+	var top23 = this.g * (this.m1 + this.m2) * Math.cos(this.t1) 
+	var top24 = this.t2V * this.t2V * this.l2 * this.m2 * Math.cos(this.t1 - this.t2)
+	
+	
+	this.t2A = (top21 * (top22 + top23 + top24))/(this.l2 * bot)//Acceleration 2 Calculated
+	
+	//Adjust Velocity and Acceleration
+	this.t1V += this.t1A
+	this.t2V -= this.t2A
+	this.t1 += this.t1V
+	this.t2 -= this.t2V
+	
+	//Position recalculated
+	this.x1 = this.l1 * Math.sin(this.t1)
+	this.y1 = this.l1 * Math.cos(this.t1)
+	
+	this.x2 = this.x1 + this.l2 * Math.sin(this.t2)
+	this.y2 = this.y1 + this.l2 * Math.cos(this.t2)
+	
+	
+}
+Wire.prototype.draw = function (ctx) {
+	ctx.beginPath()
+	ctx.translate(500,500)
+	ctx.lineWidth = 3
+	ctx.moveTo(0, 0);   
+	ctx.arc(0, 0, 5, 0, Math.PI * 2, false);
+	ctx.fill();
+	ctx.lineTo(this.x1, this.y1);  
+	ctx.arc(this.x1, this.y1, 5, 0, Math.PI * 2, false);
+	//ctx.fill();
+	ctx.moveTo(this.x1, this.y1);
+	ctx.lineTo(this.x2, this.y2); 
+    //ctx.fillStyle = "Red"
+	//ctx.arc(this.x2, this.y2, 15, 0, Math.PI * 2, false);
+	//ctx.fill();
+	ctx.stroke();          // Render the path
+	
+}
+// the "main" code begins here
+var friction = 1;
+var acceleration = 1000000;
+var maxSpeed = 200;
+
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./img/960px-Blank_Go_board.png");
+ASSET_MANAGER.queueDownload("./img/black.png");
+ASSET_MANAGER.queueDownload("./img/white.png");
+
+ASSET_MANAGER.downloadAll(function () {
+    console.log("starting up da sheild");
+    var canvas = document.getElementById('gameWorld');
+    var ctx = canvas.getContext('2d');
 
 
-AM.queueDownload("./img/minerv4.png");
-
-AM.queueDownload("./img/cave.jpg");
-AM.queueDownload("./img/board.png");
-
-AM.downloadAll(function () {
-    var canvas = document.getElementById("gameWorld");
-    var ctx = canvas.getContext("2d");
-    var count = 0;
     var gameEngine = new GameEngine();
+    
+    var wire = new Wire(gameEngine);
+    gameEngine.addEntity(wire)
+    var circle = new Circle(gameEngine, wire, 20);
+    gameEngine.addEntity(circle);
     gameEngine.init(ctx);
     gameEngine.start();
-
-    gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/cave.jpg")));
-    for(var i = 0; i < 8; i++){
-    	gameEngine.addEntity(new Platform(gameEngine, AM.getAsset("./img/board.png"), count, 530));
-    	count += 100
-    }
-    
-    gameEngine.addEntity(new Warrior(gameEngine, AM.getAsset("./img/minerv4.png")));
-
-    console.log("All Done!");
 });
